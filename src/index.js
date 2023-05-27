8efb46322efc611260a90abeda6a2dbf89e1b039
biservasilev <biservasilev94@gmail.com>
2023-05-28 01:03:28 +0300

    Add static assets

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
-
+const handlebars = require("express-handlebars");
 const app = express();
 
 const PORT = 5015;
 
+// Express config
+app.use(express.static("src/static"));
+
+// Handlebars config
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+// App routes
 app.get("/", (req, res) => {
-  res.send("Hello from Cubical");
+  res.render("index");
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
