6e0a123280e3a82dae9269dc2e23ec62521c22e8
biservasilev <biservasilev94@gmail.com>
2023-05-31 13:58:52 +0300

    Show cubes dynamically

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
+const cubeServices = require("../services/cubeService.js");
 
 router.get("/", (req, res) => {
-  res.render("index");
+  const cubes = cubeServices.getAllCubes();
+  res.render("index", { cubes });
 });
 
 router.get("/about", (req, res) => {
