02e8cc3d5bb800e2a6c88db1272f02472d92e57c
biservasilev <biservasilev94@gmail.com>
2023-06-11 12:24:18 +0300

    Implementing user logic

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,3 +1,21 @@
+const bcrypt = require("bcrypt");
+
 const User = require("../models/User.js");
 
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (username, password) => {
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    throw new Error("Wrong username or password!");
+  }
+
+  const isValid = await bcrypt.compare(password, user.password);
+
+  if (!isValid) {
+    throw new Error("Wrong username or password!");
+  }
+
+  return user;
+};
