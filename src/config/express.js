2757a91323c1d51a00cc8ff9f4df02b661e6a3c1
biservasilev <biservasilev94@gmail.com>
2023-06-11 12:42:47 +0300

    Implemanting user cookies and jwt

--- src/config/express.js
+++ src/config/express.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 
 function expressConfig(app) {
   app.use(express.static(path.resolve(__dirname, "../static")));
--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcrypt");
-
+const jwt = require("../lib/jwt.js");
 const User = require("../models/User.js");
+
+const SECRET = "sakd8dc12k09cj9asmv09j12v32103vkasjnv429";
 
 exports.register = (userData) => User.create(userData);
 
@@ -17,5 +19,12 @@ exports.login = async (username, password) => {
     throw new Error("Wrong username or password!");
   }
 
-  return user;
+  const payload = {
+    _id: user._id,
+    username: user.username,
+  };
+
+  const token = jwt.sign(payload, SECRET, { expiresIn: "2d" });
+
+  return token;
 };
